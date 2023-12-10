5e8bf79f70f5cc97de5663c07108273533637b73
Ug.V. <32958860+UgVo@users.noreply.github.com>
2023-12-10 20:00:26 +0100

    Day 03 of AoC 2023 Second part

--- 2023/03/main.cpp
+++ 2023/03/main.cpp
@@ -5,26 +5,27 @@
 #include <vector>
 #include <map>
 
-#define MAX_RED 12
-#define MAX_GREEN 13
-#define MAX_BLUE 14
-
 typedef struct {
     int value;
     size_t index_b;
     size_t index_e;
+    size_t line = 0;
 } Number;
-
 
 int sumOfNumberNearSymbol(std::string name);
 std::vector<std::string> parseInput(std::string fileName);
 int getPartNumberSum(std::vector<std::string> table);
-std::vector<Number> getLineNumbers(std::string line);
+std::vector<Number> getLineNumbers(std::string line, size_t index = 0);
 int isPartNumber(Number num, int line, std::vector<std::string> table);
+int sumOfGearRatios(std::string name);
+std::map<std::pair<int,int>,std::vector<int>> getStarsGears(std::vector<std::string> table);
+std::vector<Number> getNumbers(std::vector<std::string> table);
+std::vector<std::pair<int,int>> hasStars(Number number, std::vector<std::string> table);
 
 
 int main() {
     std::cout << sumOfNumberNearSymbol("input") << std::endl;
+    std::cout << sumOfGearRatios("input") << std::endl;
 }
 
 int sumOfNumberNearSymbol(std::string name) {
@@ -57,20 +58,20 @@ int getPartNumberSum(std::vector<std::string> table) {
     return sum;
 }
 
-std::vector<Number> getLineNumbers(std::string line) {
+std::vector<Number> getLineNumbers(std::string line, size_t index) {
     std::vector<Number> ouput;
     std::string current_number;
     for (int i = 0; i < line.size(); ++i) {
         if (isdigit(line[i])) {
             current_number += line[i];
         } else if (!current_number.empty()) {
-            Number val = {stoi(current_number),size_t(i)-current_number.size(),size_t(i)-1};
+            Number val = {stoi(current_number),size_t(i)-current_number.size(),size_t(i)-1,index};
             ouput.emplace_back(val);
             current_number.clear();
         }
     }
     if (!current_number.empty()) {
-        Number val = {stoi(current_number),line.size()-current_number.size(),line.size()-1};
+        Number val = {stoi(current_number),line.size()-current_number.size(),line.size()-1,index};
         ouput.emplace_back(val);
     }
     return ouput;
@@ -89,3 +90,53 @@ int isPartNumber(Number num, int line, std::vector<std::string> table) {
     }
     return flag ? num.value : 0;
 }
+
+int sumOfGearRatios(std::string name) {
+    std::vector<std::string> table = parseInput(name);
+    std::map<std::pair<int,int>,std::vector<int>> starsGears = getStarsGears(table);
+    int sum = 0;
+    for (auto elem : starsGears) {
+        if (elem.second.size() == 2) {
+            sum += elem.second[0]*elem.second[1];
+        }
+    }
+    return sum;
+}
+
+std::map<std::pair<int,int>,std::vector<int>> getStarsGears(std::vector<std::string> table) {
+    std::map<std::pair<int,int>,std::vector<int>> output;
+    auto numbers = getNumbers(table);
+    for (int i = 0; i < numbers.size(); ++i) {
+        auto stars = hasStars(numbers[i], table);
+        for (int j = 0; j < stars.size(); ++j) {
+            output[stars[j]].push_back(numbers[i].value);
+        }
+    }
+    return output;
+}
+
+std::vector<Number> getNumbers(std::vector<std::string> table) {
+    std::vector<Number> output;
+    for (int i = 0; i < table.size(); ++i) {
+        auto val = getLineNumbers(table[i],i);
+        output.insert(output.end(), val.begin(), val.end());
+    }
+    return output;
+}
+
+std::vector<std::pair<int,int>> hasStars(Number number, std::vector<std::string> table) {
+    std::vector<std::pair<int,int>> res;
+    int i_min = std::max<int>(number.line - 1, 0);
+    int i_max = std::min<int>(number.line + 1, table.size()-1) + 1;
+    int j_min = std::max<int>(number.index_b - 1, 0);
+    int j_max = std::min<int>(number.index_e + 1, table[0].size()-1) + 1;
+    for (int i = i_min; i < i_max; ++i ) {
+        for (int j = j_min; j < j_max; ++j) {
+            if (table[i][j] == '*') {
+                res.push_back(std::make_pair(i,j));
+            }
+        }
+    }
+    return res;
+}
+
