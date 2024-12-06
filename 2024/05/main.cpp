18a04b7b54fb4b93a47af1f0e286ce13d5f9957d
Ug.V. <32958860+UgVo@users.noreply.github.com>
2024-12-06 08:15:00 +0100

    Day 5 of AoC 2024 part 2 Forgot to commit ...
    Signed-off-by: Ug.V. <32958860+UgVo@users.noreply.github.com>

--- 2024/05/main.cpp
+++ 2024/05/main.cpp
@@ -14,14 +14,16 @@ Input parseInput(std::string fileName);
 std::pair<int, int> parseRule(std::string line);
 std::vector<int> parseUpdate(std::string line);
 
-bool checkRules(std::vector<std::pair<int, int>> rules, std::vector<int> update);
+size_t checkRules(std::vector<std::pair<int, int>> rules, std::vector<int> update);
+std::vector<int> reorder(std::vector<std::pair<int, int>> rules, std::vector<int> update);
 
 size_t grayStar(Input input);
-// size_t goldStar(Input input);
+size_t goldStar(Input input);
 
 int main(int, char *[]) {
     auto input = parseInput("input");
     std::cout << "res gray star : " << grayStar(input) << std::endl;
+    std::cout << "res gold star : " << goldStar(input) << std::endl;
 }
 
 Input parseInput(std::string fileName) {
@@ -69,29 +71,63 @@ std::vector<int> parseUpdate(std::string line) {
     return res;
 }
 
-bool checkRules(std::vector<std::pair<int, int>> rules, std::vector<int> update) {
+size_t checkRules(std::vector<std::pair<int, int>> rules, std::vector<int> update) {
     std::map<int, int> pagePosition;
-    for (int i = 0; i < 100; ++i) {
+    for (size_t i = 0; i < 100; ++i) {
         pagePosition.insert(std::make_pair(i, -1));
     }
-    for (int i = 0; i < update.size(); ++i) {
+    for (size_t i = 0; i < update.size(); ++i) {
         pagePosition[update[i]] = i;
     }
 
-    for (auto rule : rules) {
-        if (pagePosition[rule.first] != -1 && pagePosition[rule.second] != -1 &&
-            pagePosition[rule.first] > pagePosition[rule.second]) {
-            return false;
+    for (size_t i = 0; i < rules.size(); ++i) {
+        if (pagePosition[rules[i].first] != -1 && pagePosition[rules[i].second] != -1 &&
+            pagePosition[rules[i].first] > pagePosition[rules[i].second]) {
+            return pagePosition[rules[i].first];
         }
     }
-    return true;
+    return update.size();
 }
 
 size_t grayStar(Input input) {
     size_t res = 0;
     for (auto update : input.updates) {
-        if (checkRules(input.rules, update)) {
+        if (checkRules(input.rules, update) == update.size()) {
             res += update[update.size() / 2];
+        }
+    }
+    return res;
+}
+
+std::vector<int> reorder(std::vector<std::pair<int, int>> rules, std::vector<int> update) {
+    size_t index = checkRules(rules, update);
+    int value = update[index];
+    std::vector<std::vector<int>::iterator> itMajor;
+
+    update.erase(update.begin() + index);
+
+    for (int i = 0; i < rules.size(); ++i) {
+        auto it = std::find(update.begin(), update.end(), rules[i].second);
+        if (it != update.end()) {
+            itMajor.push_back(it);
+        }
+    }
+    std::sort(itMajor.begin(), itMajor.end());
+
+    update.insert(itMajor.front(), value);
+
+    if (checkRules(rules, update) != update.size()) {
+        update = reorder(rules, update);
+    }
+    return update;
+}
+
+size_t goldStar(Input input) {
+    size_t res = 0;
+    for (auto update : input.updates) {
+        if (checkRules(input.rules, update) != update.size()) {
+            auto reorderedUpdate = reorder(input.rules, update);
+            res += reorderedUpdate[reorderedUpdate.size() / 2];
         }
     }
     return res;
