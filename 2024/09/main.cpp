36ab27e65d032f7de0a877b7004a115162ce7cbb
Ug.V. <32958860+UgVo@users.noreply.github.com>
2024-12-09 16:18:24 +0100

    Day 9 of AoC part 2

--- 2024/09/main.cpp
+++ 2024/09/main.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <list>
 #include <string>
 #include <vector>
 
@@ -10,16 +12,18 @@ struct Block {
     int fileID;
     Type type;
     int size;
+    bool valid;
 };
 
 struct Input {
-    std::vector<Block> blocks;
+    std::list<Block> blocks;
     std::vector<int> memory;
 };
 
 Input parseInput(std::string fileName);
 
 std::vector<int> compactInput(std::vector<int> input);
+std::list<Block> compactInputByBlock(std::list<Block> input);
 
 size_t grayStar(Input input);
 size_t goldStar(Input input);
@@ -27,7 +31,7 @@ size_t goldStar(Input input);
 int main(int, char*[]) {
     auto input = parseInput("input");
     std::cout << "res gray star : " << grayStar(input) << std::endl;
-    //  std::cout << "res gold star : " << goldStar(input) << std::endl;
+    std::cout << "res gold star : " << goldStar(input) << std::endl;
 }
 
 Input parseInput(std::string fileName) {
@@ -41,13 +45,13 @@ Input parseInput(std::string fileName) {
             std::getline(file, line);
             for (int i = 0; i < line.size(); ++i) {
                 if (flag) {
-                    res.blocks.push_back(Block{idFile, Block::file, line[i] - '0'});
+                    res.blocks.push_back(Block{idFile, Block::file, line[i] - '0', true});
                     for (int j = 0; j < line[i] - '0'; ++j) {
                         res.memory.push_back(idFile);
                     }
                     idFile++;
                 } else {
-                    res.blocks.push_back(Block{idFile, Block::empty, line[i] - '0'});
+                    res.blocks.push_back(Block{idFile, Block::empty, line[i] - '0', true});
                     for (int j = 0; j < line[i] - '0'; ++j) {
                         res.memory.push_back(-1);
                     }
@@ -80,11 +84,50 @@ std::vector<int> compactInput(Input input) {
     return input.memory;
 }
 
+std::list<Block> compactInputByBlock(std::list<Block> input) {
+    for (auto itr = input.rbegin(); itr != input.rend(); ++itr) {
+        if (itr->type == Block::file) {
+            for (auto it = input.begin(); it != itr.base(); ++it) {
+                if (it->type == Block::empty) {
+                    if (it->size == itr->size) {
+                        input.insert(it, *itr);
+                        itr->valid = false;
+                        input.erase(it);
+                        break;
+                    } else if (it->size > itr->size) {
+                        input.insert(it, *itr);
+                        it->size -= itr->size;
+                        itr->valid = false;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return input;
+}
+
 size_t grayStar(Input input) {
     size_t res = 0;
     auto compacted = compactInput(input);
     for (int i = 0; i < compacted.size(); ++i) {
         res += i * compacted[i];
+    }
+    return res;
+}
+
+size_t goldStar(Input input) {
+    size_t res = 0;
+    size_t count = 0;
+    auto compacted = compactInputByBlock(input.blocks);
+    for (auto it = compacted.begin(); it != compacted.end(); ++it) {
+        if (it->type == Block::empty || !it->valid) {
+            count += it->size;
+        } else {
+            for (int i = 0; i < it->size; ++i) {
+                res += count++ * it->fileID;
+            }
+        }
     }
     return res;
 }
